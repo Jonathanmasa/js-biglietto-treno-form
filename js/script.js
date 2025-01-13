7abda8dcc9c14ef98d8f01f661010edad9a8455c
Jonathanmasa <jonathanmasa97@gmail.com>
2025-01-13 17:52:44 +0100

    draft[milestone1]

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// seleziono gli input
+const kilometers = parseFloat(document.getElementById("kilometers").value);
+const age = parseInt(document.getElementById("age").value);
+
+// calcolo prezzo al km
+const priceKm = 0.21;
+let priceTicket = kilometers * priceKm;
+
+// calcolo e sconti
+if (age < 18) {
+    priceTicket *= 0.8;
+} else if (age < 65) {
+    priceTicket *= 0.6;
+}
+
+// stampo il risultato in console
+console.log(`Prezzo del biglietto: ${priceTicket.toFixed(2)}`);
